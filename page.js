a6c4ddb84c34d97da01d7bd213e2dca21fac5b45
Nicholas Ford <niford98@gmail.com>
2024-12-21 23:03:45 -0600

    added and generalized routing approach

--- page.js
+++ page.js
@@ -1,10 +1,29 @@
+const DEV = true;
 const root = document.getElementById("root");
-const routePathnames = {
-  RESUME: "/",
-};
-const routes = [{ path: routePathnames.RESUME, page: resumePageView }];
 
-window.onload = function (e) {
+// Routing
+const routePathnames = {
+  HOME: "/",
+  RESUME: "/Resume",
+  CS_CAPSTONE: "/Spend-Sense-Info",
+  SE_CAPSTONE: "/Dungeon-Explorer-Info",
+};
+
+const routes = [
+  { path: routePathnames.HOME, page: loadHomePage },
+  { path: routePathnames.RESUME, page: () => loadStaticContent("RESUME") },
+  { path: routePathnames.CS_CAPSTONE, page: () => loadStaticContent("CS_CAPSTONE") },
+  { path: routePathnames.SE_CAPSTONE, page: () => loadStaticContent("SE_CAPSTONE") },
+];
+
+const pageNames = {
+  HOME: "./home.html",
+  RESUME: "./resume.html",
+  CS_CAPSTONE: "./cs_capstone.html",
+  SE_CAPSTONE: "./se_capstone.html",
+};
+
+window.onload = function(e) {
   const pathname = window.location.pathname;
   const hash = window.location.hash;
   route(pathname, hash);
@@ -23,8 +42,24 @@ function route(pathname, hash) {
   }
 }
 
-async function resumePageView() {
-  const response = await fetch("./resume.html", { cache: "no-store" });
+// Page loading functions
+async function loadStaticContent(pageName) {
+  // const pageNames prevents loading arbitrary files (sanity checking)
+  let page = pageNames[pageName];
+  if (!page) {
+    if (DEV) {
+      console.log("Called loadStaticContent with unrecognized argument ", pageName);
+    }
+    alert("Failed to locate file '" + pageName + "'; loading default...");
+    page = pageNames["RESUME"];
+  }
+  const response = await fetch(page, { cache: "no-store" });
   root.innerHTML = "";
   root.innerHTML = await response.text();
+}
+
+async function loadHomePage() {
+  await loadStaticContent("HOME");
+  const resumeButton = document.getElementById("resumeButton");
+  resumeButton.onclick = () => loadStaticContent("RESUME");
 }
